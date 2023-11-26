f803c84221a72f4bf7d585cb4f46b3898b1bbadd
p0syd0n <ajeff9752@gmail.com>
2023-11-26 03:42:14 +0000

    Added things so it works

--- index.js
+++ index.js
@@ -2,33 +2,57 @@ import express from 'express';
 import dotenv from 'dotenv';
 import { fileURLToPath } from 'url';
 import path from 'path';
-import { json } from 'body-parser';
-import { getJson } from 'serpapi';
+
+import cheerio from 'cheerio';
+
 dotenv.config();
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-const use_serp = false;
 
 const port = process.env.PORT;
-const serp_api_key = process.env.SERP_API_KEY
 const app = express();
 
+async function duckDuckGo(query) {
+    try {
+        const response = await fetch(`https://html.duckduckgo.com/html/?q=${query}`);
+        if (!response.ok) throw new Error(`Failed to fetch search results. Status: ${response.status}`);
+
+        const html = await response.text();
+        const $ = cheerio.load(html);
+
+        const results = $('.result.results_links.results_links_deep.web-result');
+        const topResults = [];
+
+        results.slice(0, 20).each((index, element) => {
+            const title = $(element).find('.result__title a').text();
+            const url = $(element).find('.result__url').text().trim();
+            const description = $(element).find('.result__snippet').text();
+            topResults.push({ title, url, description });
+        });
+
+        return topResults;
+    } catch (error) {
+        console.error('Error fetching DuckDuckGo search results:', error.message);
+        throw error;
+    }
+}
+
 async function collectSearchResults(query) {
-  let googleJson, bingJson, yahooJson, yandexJson;
-  if (use_serp) {
-    getJson({
-      q: query,
-      location: "United States",
-      hl: "en",
-      gl: "us",
-      google_domain: "google.com",
-      api_key: serp_api_key
-    }, (json) => {
-      googleJson = json;
-    });
-  }
-  return json; // {item: {url: url, name: name, description: description}}
+  const duckDuckGoResults = await duckDuckGo(query);
+  // Add more functions for other search engines or sources
+  // const googleResults = await getGoogleResults(query);
+  // const bingResults = await getBingResults(query);
+
+  // Format results from different sources
+  const formattedResults = [
+      ...duckDuckGoResults.map(({ title, url, description }) => ({ title, url, description })),
+      // Add formatted results for other sources
+      // ...googleResults.map(({ title, url, description }) => ({ title, url, description })),
+      // ...bingResults.map(({ title, url, description }) => ({ title, url, description })),
+  ];
+
+  return formattedResults;
 }
 
 app.use(express.urlencoded({ extended: true }));
@@ -41,11 +65,16 @@ app.get('/', (req, res) => {
   res.render('main');
 });
 
-app.get('/search', (req, res) => {
-  console.log(req.query.q);
-  res.render('search_results')
+app.get('/search', async (req, res) => {
+    try {
+        const results = await collectSearchResults(req.query.q);
+        res.render('search_results', { results, query: req.query.q });
+    } catch (error) {
+        console.error('Error collecting search results:', error.message);
+        res.status(500).send('Internal Server Error');
+    }
 });
 
-app.listen(port, () => {
-  console.log("started on port " + port);
+app.listen(port, async () => {
+  console.log(`Server started on port ${port}`);
 });
